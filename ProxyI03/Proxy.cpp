626fdf6b1ca09e33cbb42f5366a73ed97c502a6f
xilko.2020 <xilko.2020@CLOUD-RDHOST1>
2023-03-29 15:23:52 +0700

    Добавьте файлы проекта.

---
+++ ProxyI03/Proxy.cpp
@@ -0,0 +1,36 @@
+#include "Proxy.h"
+
+Proxy::Proxy(Subject* subject, string role)
+{
+	this->subject = subject;
+	this->role = role;
+}
+
+void Proxy::Create(string key, string value)
+{
+	if (role == "Owner")
+		subject->Create(key, value);
+	else
+		throw std::logic_error("Create: Access denide.");
+}
+
+string Proxy::Read(string key)
+{
+	return subject->Read(key);
+}
+
+bool Proxy::Update(string key, string value)
+{
+	if (role != "User")
+		return subject->Update(key, value);
+	else
+		throw std::logic_error("Update: Access denide.");
+}
+
+bool Proxy::Delete(string key)
+{
+	if (role != "Administrator" || role == "Owner")
+		return subject->Delete(key);
+	else
+		throw std::logic_error("Delete: Access denide.");
+}
